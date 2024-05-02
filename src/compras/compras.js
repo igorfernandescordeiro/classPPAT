b481ad2b98dacf0f45fa04c6750fa85a601d9e94
Igor Cordeiro <ifcigor@gmail.com>
2024-05-01 22:19:32 -0300

    Criação do arquivo compras.js para execução dos testes contido no compras.test.js

--- src/compras/compras.js
+++ src/compras/compras.js
@@ -1,5 +1,26 @@
 const calcularTotal = (ferramentas, comprar) => {
+    let nomesFerramentas = [];
+    let precoTotal = 0;
 
+    if (ferramentas.length === 0 || comprar.length === 0) {
+        throw new Error("Ambas as listas precisam ter ao menos um item.");
+    };
+
+    for (let indexFerramentas = 0; indexFerramentas < ferramentas.length; indexFerramentas++) {
+        for (let indexComprar = 0; indexComprar < comprar.length; indexComprar++) {
+
+            if (ferramentas[indexFerramentas].nome === comprar[indexComprar]) {
+                nomesFerramentas[nomesFerramentas.length] = ferramentas[indexFerramentas].nome;
+                precoTotal += ferramentas[indexFerramentas].preco;
+            };
+
+        };
+    };
+
+    if (nomesFerramentas.length > 0) {
+        return `O valor a pagar pelas ferramentas (${nomesFerramentas.join(', ')}) é R$ ${precoTotal.toFixed(2)}`;
+    };
+    throw new Error('Nenhuma ferramenta desejada encontrada.');
 };
 
 module.exports = {
